f7d065ab44038e454d62a72bf02828001c848782
blakelead <adel.abdelhak.lille@gmail.com>
2020-11-25 23:40:54 +0100

    Get location from resource group

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_public_ip" "public_ip" {
   name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
 
   allocation_method   = var.allocation_method
   sku = var.sku
