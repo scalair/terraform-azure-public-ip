741ad14f55f885096b1166c50248b5e10ceac35c
Adel Abdelhak <6298183+blakelead@users.noreply.github.com>
2020-12-02 16:26:43 +0100

    add: optional domain_name_label attribute

--- main.tf
+++ main.tf
@@ -6,11 +6,10 @@ resource "azurerm_public_ip" "public_ip" {
   name                = var.name
   resource_group_name = data.azurerm_resource_group.resource_group.name
   location            = data.azurerm_resource_group.resource_group.location
-
   allocation_method   = var.allocation_method
-  sku = var.sku
-
-  ip_version = var.ip_version
+  sku                 = var.sku
+  ip_version          = var.ip_version
+  domain_name_label   = var.domain_name_label == "" ? null : var.domain_name_label
 
   tags = var.tags
 }
